5130625eeaf0649bd5e3b8f84c65ad5da37a134c
svasilchenko <liketravelin@gmail.com>
2023-07-12 23:18:57 +0300

    add tests

--- test_reqres.py
+++ test_reqres.py
@@ -1,3 +1,4 @@
+import pytest
 from jsonschema.validators import validate
 
 from helper import load_json_schema, reqres_session
@@ -5,11 +6,13 @@ from helper import load_json_schema, reqres_session
 
 def test_requested_page_number():
     page = 2
+    schema = load_json_schema('get_user_list.json')
 
     response = reqres_session.get('/api/users', params={'page': page})
 
     assert response.status_code == 200
     assert response.json()['page'] == page
+    validate(response.json(), schema=schema)
 
 
 def test_users_list_default_length():
@@ -52,8 +55,6 @@ def test_create_user_schema_validation():
             "name": name,
             "job": job}
     )
-    response.json()
-
 
     validate(instance=response.json(), schema=schema)
 
@@ -63,3 +64,91 @@ def test_delete_user_returns_204():
 
     assert response.status_code == 204
     assert response.text == ''
+
+
+@pytest.mark.parametrize('id', (1, 2, 3, 4, 5))
+def test_get_single_user(id):
+    schema = load_json_schema('get_single_user.json')
+
+    response = reqres_session.get(f'/api/users/{id}')
+
+    validate(response.json(), schema=schema)
+    assert response.json()['data']['id'] == id
+
+
+def test_resources_list_default_length():
+    default_resources_count = 6
+    schema = load_json_schema('get_resources_list.json')
+
+    response = reqres_session.get('/api/unknown')
+
+    assert len(response.json()['data']) == default_resources_count
+    validate(response.json(), schema=schema)
+
+
+@pytest.mark.parametrize('id', (1, 2, 3, 4, 5))
+def test_get_single_resource(id):
+    schema = load_json_schema('get_single_resource.json')
+
+    response = reqres_session.get(f'/api/unknown/{id}')
+
+    validate(response.json(), schema=schema)
+    assert response.json()['data']['id'] == id
+
+
+@pytest.mark.parametrize('method', ('put', 'patch'))
+def test_update_user(method):
+    schema = load_json_schema('update_single_user.json')
+    request_body = {"name": "morpheus", "job": "zion resident"}
+
+    response = reqres_session.request(method=method, url=f'/api/unknown/{id}', json=request_body)
+
+    validate(response.json(), schema=schema)
+    assert response.json()['name'] == request_body['name']
+    assert response.json()['job'] == request_body['job']
+
+
+def test_successful_register():
+    schema = load_json_schema('post_register_user.json')
+    request_body = {"email": "eve.holt@reqres.in", "password": "pistol"}
+
+    response = reqres_session.post(url='/api/register', json=request_body)
+
+    validate(response.json(), schema=schema)
+
+
+def test_unsuccesful_register():
+    request_body = {"email": "sydney@fife"}
+
+    response = reqres_session.post(url='/api/register', json=request_body)
+
+    assert response.json()['error'] == 'Missing password'
+
+
+def test_succesful_login():
+    schema = load_json_schema('post_login_user.json')
+    request_body = {"email": "eve.holt@reqres.in", "password": "cityslicka"}
+
+    response = reqres_session.post(url='/api/login', json=request_body)
+
+    validate(response.json(), schema=schema)
+    assert response.json()['token'] == 'QpwL5tke4Pnpja7X4'
+
+
+def test_unsuccesful_login():
+    request_body = {"email": "peter@klaven"}
+
+    response = reqres_session.post(url='/api/login', json=request_body)
+
+    assert response.json()['error'] == 'Missing password'
+
+
+def test_users_page_with_delay():
+    delay = 3
+    schema = load_json_schema('get_user_list.json')
+
+    response = reqres_session.get('/api/users', params={'delay': delay})
+
+    assert response.status_code == 200
+    assert response.elapsed.total_seconds() >= 3
+    validate(response.json(), schema=schema)
